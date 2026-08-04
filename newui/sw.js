6e0d74c238eac36a009510a8b3abd6054488a730
Valerio Di Zio <73752271+v4l3rio@users.noreply.github.com>
2026-08-04 19:57:26 +0200

    Add experimental new UI

---
+++ newui/sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'lp-counter-newui-v1';
+const APP_SHELL = [
+    './',
+    './index.html',
+    './styles.css?v=1',
+    './app.js?v=1',
+    './manifest.webmanifest',
+    './icon.svg',
+    '../apple_icon.png',
+    '../favicon.ico',
+    '../lifedrop_sound.mp3'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_SHELL))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    if (request.method !== 'GET' || new URL(request.url).origin !== self.location.origin) return;
+
+    if (request.mode === 'navigate') {
+        event.respondWith(
+            fetch(request)
+                .then(response => {
+                    const copy = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy));
+                    return response;
+                })
+                .catch(() => caches.match('./index.html'))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(request).then(cached => cached || fetch(request).then(response => {
+            if (response.ok) {
+                const copy = response.clone();
+                caches.open(CACHE_NAME).then(cache => cache.put(request, copy));
+            }
+            return response;
+        }))
+    );
+});
